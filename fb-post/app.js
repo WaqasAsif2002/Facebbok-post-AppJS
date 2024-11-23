e87093b94a3652882feb87e07f2c1a025ca6b08f
Mohammad Waqas <117596916+WaqasAsif2002@users.noreply.github.com>
2024-11-23 18:01:59 +0500

    Update app.js

--- fb-post/app.js
+++ fb-post/app.js
@@ -1,7 +1,7 @@
 function cart() {
-   var name = document.getElementById('username').value;  // name
-   var title = document.getElementById('title').value;    // title
-   var desc = document.getElementById('desc').value;      // description
+   var name = document.getElementById('username');  // name
+   var title = document.getElementById('title');    // title
+   var desc = document.getElementById('desc');      // description
    var currentDateTime = new Date().toUTCString();
    var listingContainer = document.querySelector("#listingContainer");
 
@@ -9,19 +9,22 @@ function cart() {
    listingContainer.innerHTML += 
    `<div class="card text-center">
   <div class="card-header">
-    Name is : ${name}
+    Name is : ${name.value}
   </div>
   <div class="card-body">
-    <h5 class="card-title">${title}</h5>
-    <p class="card-text">${desc}.</p>
+    <h5 class="card-title">${title.value}</h5>
+    <p class="card-text">${desc.value}.</p>
     <a href="https://github.com/WaqasAsif2002" class="btn btn-primary">Goto Github</a>
   </div>
   <div class="card-footer text-body-secondary">
 ${currentDateTime}
-  </div>
+</div>
 </div>
    `;
+   name.value =''
+   title.value =''
+   desc.value =''
   
-
+   
 }
 
